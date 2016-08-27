8e91a7a48ca3493207832db0cfb446a1462f628d
andsfonseca <andsf@live.com>
2016-08-27 15:06:13 -0300

    [Changelog]
    [ADD] Adicionado funções da Manager para Salas
    [MOD] Small fixes

--- src/aplicacao/manager/SalasManager.java
+++ src/aplicacao/manager/SalasManager.java
@@ -6,13 +6,21 @@ import entidades.Sala;
 import util.DAO;
 
 /**
- * Classe para controle de Salas
+ * Classe para controle de Salas.
  *
  */
 public class SalasManager {
 	
 	private static DAO<Sala> dao = new DAO<Sala>(Sala.class);
 	
+	/**
+	 * Cria uma entidade de Sala e persiste no banco.
+	 * 
+	 * @param nome Nome da Sala.
+	 * @param nCol Número máximo de Colunas da Sala.
+	 * @param nLin Número máximo de Linhas da Sala.
+	 * @return Retorna verdadeiro se a sala foi criada.
+	 */
 	public static boolean criarSala(String nome, int nCol, int nLin){
 		if(nCol > 0  && nLin > 0){
 			Sala sala = new Sala(nome, nCol, nLin);
@@ -23,8 +31,29 @@ public class SalasManager {
 			
 	}
 	
-	//TODO: Atualizar Sala
-	//TODO: Remover Sala
+	/**
+	 * Remove uma sala e persiste no Banco.
+	 * @param sala Sala a ser removida.
+	 */
+	public static void removerSala(Sala sala){
+		dao.remover(sala);
+	}
+	
+	/**
+	 * Atualiza uma sala e persiste no Banco.
+	 * @param sala Sala a ser atualizada.
+	 */
+	public static void atualizarSala(Sala sala){
+		dao.atualizar(sala);
+	}
+	
+	/**
+	 * Retorna uma Lista com as Salas salvas no Banco.
+	 * @return Lista de salas salvas.
+	 */
+	public static List<Sala> listarSalas(){
+		return dao.listar();
+	}
 	//TODO: Listar Sala
 	
 }
