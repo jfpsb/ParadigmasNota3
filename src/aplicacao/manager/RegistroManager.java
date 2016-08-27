8b05920c51310a39ba5a8355b1764e9735973f40
Felipe Borges <jfpsb_felipe2@hotmail.com>
2016-08-27 20:21:08 -0300

    Entidade de Registros(logs)

---
+++ src/aplicacao/manager/RegistroManager.java
@@ -0,0 +1,47 @@
+package aplicacao.manager;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import entidades.Filme;
+import entidades.Registro;
+import util.DAO;
+
+/**
+ * Classe para controle de registros.
+ * 
+ * @author jfpsb
+ *
+ */
+public class RegistroManager {
+	private static DAO<Registro> dao = new DAO<Registro>(Registro.class);
+
+	/**
+	 * Cria objeto de registro e salva no banco.
+	 * 
+	 * @param msg
+	 *            Mensagem do registro.
+	 * @param data
+	 *            Data em que registro foi feito.
+	 * @return True se a inserção foi bem sucedida, false se não.
+	 */
+	public static boolean criarRegistro(String msg, Timestamp data) {
+
+		if (!msg.isEmpty()) {
+			Registro registro = new Registro(msg, data);
+			dao.salva(registro);
+			return true;
+		}
+
+		return false;
+	}
+	
+	/**
+	  * Recupera a lista de registros.
+	  * 
+	  * @return Lista de registros.
+	  */
+	public static List<Registro> listarFilmes(){
+		return dao.listar();
+	}
+}
