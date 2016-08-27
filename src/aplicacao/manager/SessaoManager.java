7d5b504b8376b7825f6659a8390033e23a818709
andsfonseca <andsf@live.com>
2016-08-27 16:57:07 -0300

    [Changelog]
    [ADD] Criado Entidade de Sessão
    [ADD] Criada Classe de SessaoManager
    [ADD] TODO para SessaoManager

---
+++ src/aplicacao/manager/SessaoManager.java
@@ -0,0 +1,24 @@
+package aplicacao.manager;
+
+/**
+ * Classe para controle de sessão
+ *
+ */
+public class SessaoManager {
+	
+	//TODO: Criar Sessão (Se não existir sessão no mesmo horario)
+	//TODO: Remover Sessão
+	//TODO: Atualizar Sessão
+	//TODO: Listar Sessão
+	//TODO: Cria Reserva para a Sessao (Apenas se a sessão ainda não ocorreu) e retorna a mesma (Para registrar no ingresso)
+	//TODO: Informar quando a Sessao termina (Data de Inicio + duracao do filme)
+	//TODO: Listar as sessões existentes no horário definido
+	//TODO: Listar as sessões de um filme especifico;
+	//TODO: Listar as sessões de uma sala;
+	//TODO: Contar quantas reservas existem para a sessão
+	//TODO: Contar quantas reservas ainda são suportadas para a sessão.
+	//TODO: Remover Reserva para uma sessão (Buscar e remover em vez de enviar a sessão)
+	//TODO: Remover Sessao especifica
+	
+	
+}
